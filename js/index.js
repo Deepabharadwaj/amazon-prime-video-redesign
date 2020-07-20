7cc8fa0b6a6cacad9ffee48c1d49d79a543c8bbb
Deepa Bharadwaj <deepb@umich.edu>
2020-07-20 00:59:23 -0400

    added basic carousel

--- js/index.js
+++ js/index.js
@@ -9,3 +9,17 @@ function image_toggle(location, image1, image2){
 function image_swap(location, image){ 
     $(location).attr("src", image);
 }
+
+function image_on_out(img_id) {
+    var item = document.getElementById(img_id);
+    var img_file = ($(item).attr("src"));
+    var res = img_file.replace("hover", "default");
+    $(item).attr("src", res);
+}
+
+function image_on_hover(img_id) {
+    var item = document.getElementById(img_id);
+    var img_file = ($(item).attr("src"));
+    var res = img_file.replace("default", "hover");
+    $(item).attr("src", res);
+}
